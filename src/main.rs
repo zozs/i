81b5bba27b9d04d32b78eaf0a422fff5034d6593
Linus Karlsson <linus@zozs.se>
2020-07-13 18:47:51 +0200

    Responds with url-encoded URL :tada:

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-use std::io::Write;
-
 use actix_multipart::Multipart;
 use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer, Responder};
 use futures::{StreamExt, TryStreamExt};
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::io::Write;
 use std::path::Path;
 use std::ffi::OsStr;
 
 pub struct FileUpload {
-    original_filename_path: String,
+    original_filename: String,
+    random_filename: String,
     random_filename_path: String,
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Options {
-    use_original_filename: bool,
+    #[serde(default)]
+    use_original_filename: bool, // default for bool is false.
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct UploadResponse {
+    url: String
 }
 
 async fn index() -> impl Responder {
@@ -34,8 +42,17 @@ fn generate_random_filename(extension: Option<&str>) -> String {
     }
 }
 
-fn filename_path(filename: &str) -> String {
-    return format!("./tmp/{}", sanitize_filename::sanitize(&filename));
+fn filename_path(filename: &str) -> Result<String, Error> {
+    Ok(format!("{}/{}", get_base_dir()?, sanitize_filename::sanitize(&filename)))
+}
+
+fn get_base_dir() -> std::io::Result<&'static str> {
+    let base_dir = option_env!("I_BASE_DIR").unwrap_or("./tmp");
+
+    // Create directory where files should be uploaded.
+    std::fs::create_dir_all(base_dir)?;
+
+    Ok(base_dir)
 }
 
 fn get_extension_from_filename(filename: &str) -> Option<&str> {
@@ -44,31 +61,47 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
         .and_then(OsStr::to_str)
 }
 
-async fn parse_field_options(field: actix_multipart::Field) -> Options {
-    Options { use_original_filename: false }
+async fn parse_field_options(mut field: actix_multipart::Field) -> Result<Options, Error> {
+    // Parse data in options json.
+
+    // First read multipart data to Vec<u8>.
+    let mut v: Vec<u8> = Vec::new();
+    while let Some(chunk) = field.next().await {
+        let data = chunk.unwrap();
+        // filesystem operations are blocking, we have to use threadpool
+        v = web::block(move || v.write_all(&data).map(|_| v)).await?;
+    }
+
+    Ok(serde_json::from_slice(&v)?)
+}
+
+fn public_path(filename: &str) -> Result<String, url::ParseError> {
+    let port = option_env!("I_PORT").unwrap_or("8088");
+    let public_base_string = match option_env!("I_PUBLIC_BASE") {
+        Some(s) => s.to_string(),
+        None => format!("http://localhost:{}", port),
+    };
+
+    let public_base = url::Url::parse(&public_base_string)?;
+    Ok(public_base.join(filename)?.into_string())
 }
 
 async fn handle_upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
     let mut file_field: Option<FileUpload> = None;
     let mut options_field: Option<Options> = None;
 
-    log::debug!("handle upload!");
-
     // iterate over multipart stream
     while let Ok(Some(mut field)) = payload.try_next().await {
-        log::debug!("got before if!");
         if let Some(content_disposition) = field.content_disposition() {
             // Check if it is file or data part of form.
-            log::debug!("got disposition name {}!", content_disposition.get_name().unwrap());
-
             match content_disposition.get_name() {
                 Some("file") => {
                     // Save to temporary filename, we might later rename it to original.
                     let original_filename = content_disposition.get_filename().unwrap();
                     let extension = get_extension_from_filename(original_filename);
-                    let filename = generate_random_filename(extension);
+                    let random_filename = generate_random_filename(extension);
 
-                    let filepath = filename_path(&filename);
+                    let filepath = filename_path(&random_filename)?;
                     let random_filename_path = filepath.clone();
                     // File::create is blocking operation, use threadpool
                     let mut f = web::block(|| std::fs::File::create(filepath))
@@ -82,31 +115,40 @@ async fn handle_upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
                     
                     }
                     file_field = Some(FileUpload {
-                        original_filename_path: filename_path(original_filename),
+                        original_filename: original_filename.to_string(),
+                        random_filename,
                         random_filename_path,
                     });
                 }
-                Some("options") => options_field = Some(parse_field_options(field).await),
-                _ => { /* show error or something */ }
+                Some("options") => options_field = parse_field_options(field).await.ok(),
+                _ => { /* TODO: show error or something */ }
             }
-
-            log::debug!("gotted disposition name {}!", content_disposition.get_name().unwrap());
         } else {
-            log::debug!("no content disposition for you :(");
+            log::debug!("no content disposition in field :(");
         }
     }
 
-    log::debug!("done with parts!");
-
     // Check if we received both file itself and data.
     if let (Some(file), Some(options)) = (file_field, options_field) {
-        log::debug!("found all parts!");
-        if options.use_original_filename {
-            // Rename from temporary random filename to original.
-            std::fs::rename(file.random_filename_path, file.original_filename_path);
-        }
+        let final_filename: &str = if options.use_original_filename {
+            // Rename from temporary random filename to original. Will overwrite if filename already exists.
+            let original_filename_path = filename_path(&file.original_filename)?;
+            std::fs::rename(&file.random_filename_path, &original_filename_path)?;
+            &file.original_filename
+        } else {
+            &file.random_filename
+        };
 
-        Ok(HttpResponse::Ok().into())
+        // Derive url of newly created file.
+        let url = public_path(final_filename).map_err(|_| HttpResponse::InternalServerError())?;
+
+        Ok(
+            HttpResponse::SeeOther()
+                .header("Location", url.as_str())
+                .json(UploadResponse {
+                    url
+                })
+        )
     } else {
         Ok(HttpResponse::BadRequest().into())
     }
@@ -117,17 +159,25 @@ async fn handle_upload(mut payload: Multipart) -> Result<HttpResponse, Error> {
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
-    // Create directory where files should be uploaded.
-    std::fs::create_dir_all("./tmp").unwrap();
+    let host = "0.0.0.0";
+    let port = option_env!("I_PORT").unwrap_or("8088");
+    let bind_string = format!("{}:{}", host, port);
 
-    HttpServer::new(|| {
-        App::new().wrap(middleware::Logger::default()).service(
+    let base_dir = get_base_dir()?;
+
+    log::info!("listening on {}", bind_string);
+    log::info!("serving and storing files in: {}", base_dir);
+
+    HttpServer::new(move || {
+        App::new().wrap(middleware::Logger::default())
+        .service(
             web::resource("/")
                 .route(web::get().to(index))
                 .route(web::post().to(handle_upload)),
         )
+        .service(actix_files::Files::new("/", base_dir))
     })
-    .bind("0.0.0.0:8088")?
+    .bind(bind_string)?
     .run()
     .await
 }
